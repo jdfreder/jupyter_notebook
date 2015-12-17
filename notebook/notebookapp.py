c10d4662922d4959d4ad84982f7516099759f7a9
Brian E. Granger <ellisonbg@gmail.com>
2015-12-16 22:32:05 -0800

    Adding prototype nbextensions argument to NotebookApp

--- notebook/notebookapp.py
+++ notebook/notebookapp.py
@@ -196,6 +196,8 @@ class NotebookWebApplication(web.Application):
             iopub_data_rate_limit=ipython_app.iopub_data_rate_limit,
             rate_limit_window=ipython_app.rate_limit_window,
 
+            nbextensions=ipython_app.nbextensions,
+            
             # authentication
             cookie_secret=ipython_app.cookie_secret,
             login_url=url_path_join(base_url,'/login'),
@@ -657,6 +659,9 @@ class NotebookApp(JupyterApp):
         else:
             path.append(os.path.join(get_ipython_dir(), 'nbextensions'))
         return path
+
+    nbextensions = List(Unicode(), config=True,
+        help="A list of nbextensions to enable.")
 
     websocket_url = Unicode("", config=True,
         help="""The base URL for websockets,
