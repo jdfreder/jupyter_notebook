f8c3d9df16235dd3f7a975f574483ee4f128be35
Brian E. Granger <ellisonbg@gmail.com>
2015-12-28 20:47:04 -0800

    Working on logging.

--- notebook/static/base/js/utils.js
+++ notebook/static/base/js/utils.js
@@ -856,7 +856,6 @@ define([
     }
 
     var utils = {
-        extensions_loaded: extensions_loaded,
         load_extension: load_extension,
         load_extensions: load_extensions,
         filter_extensions: filter_extensions,
